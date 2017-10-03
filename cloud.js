871a302608923866c8893fcc5eb282ea014c6693
tonyYo <420156367@qq.com>
2017-10-03 18:40:42 +0800

    iCard添加了计算功能

--- cloud.js
+++ cloud.js
@@ -2,26 +2,8 @@ var AV = require('leanengine');
 var _ = require('underscore');
 
 // 声明 Task
-var Task = AV.Object.extend('Task');
 
-/**
- * 一个简单的云代码方法
- */
-AV.Cloud.define('hello', function(req, res) {
-  console.log(req);
-  res.success('Hello world,Tony!');
-});
 
-AV.Cloud.define('whoami', function(req, res) {
-  console.log('whoami:', req.currentUser);
-  var username = req.currentUser && req.currentUser.get('username');
-  res.success(username);
-});
-
-AV.Cloud.define('noFetchUser', {fetchUser: false}, function(req, res) {
-  console.log('noFetchUser: user=%s, sessionToken=%s', req.currentUser, req.sessionToken);
-  res.success();
-});
 
 // 从 content 中查找 tag 的正则表达式
 var tagRe = /#(\w+)/g;
@@ -39,7 +21,39 @@ AV.Cloud.beforeSave('Todo', function(req, res) {
 });
 
 
-const classNames = ['iCard','iDo','iUse']
+const iUse = 'iUse'
+const iCard = 'iCard'
+const iDo = 'iDo'
+
+
+
+AV.Cloud.afterSave(iUse,req => {
+  const query = new AV.Query(iCard);
+  return query.get(req.object.get(iCard).id).then(function(card) {
+    card.increment('useNum');
+    return card.save();
+  });
+})
+
+AV.Cloud.afterDelete(iUse,req => {
+  const query = new AV.Query(iCard);
+  return query.get(req.object.get(iCard).id).then(function(card) {
+    card.increment('useNum',-1);
+    return card.save();
+  });
+})
+
+
+const normalACL = (currentUser)=>{
+  const acl = new AV.ACL()
+  acl.setPublicReadAccess(true);
+  //说明
+  // acl.setRoleWriteAccess('Administrator',true);
+  acl.setWriteAccess(currentUser, true);
+  return acl
+}
+
+const classNames = [iCard,iUse,iDo]
 const ACLSet = (classNames)=>{
   classNames.forEach(className =>{
     setNormalACL(className)
@@ -50,12 +64,7 @@ const setNormalACL = (className)=>{
   AV.Cloud.beforeSave(className,req => new Promise((solve, reject)=>{
     const {object,currentUser} = req
     if(object){
-      const acl = new AV.ACL()
-      acl.setPublicReadAccess(true);
-      //说明
-      // acl.setRoleWriteAccess('Administrator',true);
-      acl.setWriteAccess(currentUser, true);
-      object.setACL(acl);
+      object.setACL(normalACL(currentUser));
       solve()
     }else {
       reject('未发现有效的'+className+'对象')
@@ -65,59 +74,6 @@ const setNormalACL = (className)=>{
 
 ACLSet(classNames)
 
-/**
-云函数超时示例
-https://leancloud.cn/docs/leanengine_cloudfunction_guide-node.html#超时的处理方案
-示例中写了一个没有具体业务场景的任务。
-写自己的具体业务场景下的任务时，我们建议设计成幂等任务，使其重复执行也不会有问题。
-*/
-function doTask() {
-  console.log('begin task');
-  return new Promise(function(resolve, reject) {
-    // 随机运行时长 1s ~ 20s 之间
-    var randomTime = (Math.floor(Math.random() * 20 + 1)) * 1000;
-    setTimeout(function () {
-      // 随机时长为奇数时，模拟任务失败
-      if (randomTime % 2000 === 0) {
-        resolve();
-      } else {
-        reject(new Error('some reasons for task failed'));
-      }
-    }, randomTime);
-  });
-  
-}
 
-AV.Cloud.define('asyncTask', function(req, res) {
-  var task = new Task();
-  var taskName = req.params.name;
-  // 存储任务队列，这里只设置了 name 字段，可以根据业务需求增加其他的字段信息
-  task.set('name', taskName);
-  // 设置状态为「处理中」
-  task.set('status', 'pending');
-  task.save().then(function (task) {
-    // 先返回任务 Id，再执行任务
-    res.success(task.id);
-    doTask().then(function() {
-      // 任务成功完成，设置状态为「成功」
-      task.set('status', 'success');
-      return task.save();
-    }).then(function(_task) {
-      console.log('task succeed');
-    }).catch(function (error) {
-      // 任务失败，设置状态为「失败」
-      task.set('status', 'failure');
-      task.set('errMsg', error.message);
-      task.save().then(function(_task) {
-        console.log('task failed');
-      }).catch(function(error) {
-        console.log('更新 task 失败', error);
-      });
-    });
-  }).catch(function(error) {
-    // 任务队列保存失败，返回失败
-    res.err(error);
-  });
-});
 
 module.exports = AV.Cloud;
