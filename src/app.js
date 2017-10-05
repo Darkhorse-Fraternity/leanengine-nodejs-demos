9efac1723a7d4bc70f4f83f8ba2ea97576514fad
tonyYo <420156367@qq.com>
2017-10-05 15:42:12 +0800

    no message

--- src/app.js
+++ src/app.js
@@ -1,78 +1,76 @@
+
 'use strict';
 
-var express = require('express');
-var timeout = require('connect-timeout');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var AV = require('leanengine');
+import path from 'path';
+// import { Server } from 'https';
+import Express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+import routes from './routes';
+import NotFoundPage from './components/NotFoundPage';
+import timeout from 'connect-timeout'
+import AV from 'leanengine'
+import cookieParser  from 'cookie-parser'
+import bodyParser from 'body-parser'
+import cloud from './cloud'
 
-// 加载云函数定义，你可以将云函数拆分到多个文件方便管理，但需要在主文件中加载它们
-require('./cloud');
 
-var app = express();
-
-// 设置模板引擎
+export const app = new Express();
+// const server = new Server(app);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+// define the folder that will be used for static assets
+app.use(Express.static(path.join(__dirname, 'static')));
+
+// universal routing and rendering
+
 
 // 设置默认超时时间
 app.use(timeout('15s'));
 
+
 // 加载云引擎中间件
 app.use(AV.express());
+
 
 app.enable('trust proxy');
 // 需要重定向到 HTTPS 可去除下一行的注释。
 app.use(AV.Cloud.HttpsRedirect());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 
-app.get('/', function(req, res) {
-  res.render('index', { currentTime: new Date() });
+app.get('*', (req, res) => {
+  match(
+    { routes, location: req.url },
+    (err, redirectLocation, renderProps) => {
+
+      // in case of error display the error message
+      if (err) {
+        return res.status(500).send(err.message);
+      }
+
+      // in case of redirect propagate the redirect to the browser
+      if (redirectLocation) {
+        return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+      }
+
+      // generate the React markup for the current route
+      let markup;
+      if (renderProps) {
+        // if the current route matched we have renderProps
+        markup = renderToString(<RouterContext {...renderProps}/>);
+      } else {
+        // otherwise we can render a 404 page
+        markup = renderToString(<NotFoundPage/>);
+        res.status(404);
+      }
+
+      // render the index template with the embedded React markup
+      return res.render('index', { markup });
+    }
+  );
 });
-
-// 可以将一类的路由单独保存在一个文件中
-app.use('/todos', require('./routes/todos'));
-
-app.use(function(req, res, next) {
-  // 如果任何一个路由都没有返回响应，则抛出一个 404 异常给后续的异常处理器
-  if (!res.headersSent) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  }
-});
-
-// error handlers
-app.use(function(err, req, res, next) {
-  if (req.timedout && req.headers.upgrade === 'websocket') {
-    // 忽略 websocket 的超时
-    return;
-  }
-
-  var statusCode = err.status || 500;
-  if (statusCode === 500) {
-    console.error(err.stack || err);
-  }
-  if (req.timedout) {
-    console.error('请求超时: url=%s, timeout=%d, 请确认方法执行耗时很长，或没有正确的 response 回调。', req.originalUrl, err.timeout);
-  }
-  res.status(statusCode);
-  // 默认不输出异常详情
-  var error = {};
-  if (app.get('env') === 'development') {
-    // 如果是开发环境，则将异常堆栈输出到页面，方便开发调试
-    error = err;
-  }
-  res.render('error', {
-    message: err.message,
-    error: error
-  });
-});
-
-module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,18 +1,24 @@
 'use strict';
 
-var AV = require('leanengine');
+import { Server } from 'http';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import AV from 'leanengine'
+import cloud from './cloud'
+import {app} from './app'
 
-AV.init({
-  appId: process.env.LEANCLOUD_APP_ID,
-  appKey: process.env.LEANCLOUD_APP_KEY,
-  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
-});
+if(process.env.LEANCLOUD_APP_ID){
+  AV.init({
+    appId: process.env.LEANCLOUD_APP_ID,
+    appKey: process.env.LEANCLOUD_APP_KEY,
+    masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+  });
+}
 
-var app = require('./app');
 
-// 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
-// LeanEngine 运行时会分配端口并赋值到该变量。
-var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000);
+
+// start the server
+const PORT = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000);
 
 app.listen(PORT, function (_err) {
   console.log('Node app is running on port:', PORT);
